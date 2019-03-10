9305a66e17b8b83dce29466c65d179ec65a7392c
osmannamso123@gmail.com <osmannamso123@gmail.com>
2019-03-10 20:30:25 +0600

    asd

--- aibolat/settings.py
+++ aibolat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$tmf#*p&5uum=xy)ng@7mav-k4!pp#w_e+wgffyqu9tcn4ds)l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
